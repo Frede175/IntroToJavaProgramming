9b9c368f2153f36533f510865933042f04e9945c
Frede175 <fsr1998@hotmail.com>
2017-10-02 16:01:29 +0200

    Bugs has been fixed

---
+++ src/fred/Chapter10/TestClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fred.Chapter10;
+
+/**
+ *
+ * @author fsr19
+ */
+public class TestClass implements Test{
+
+    @Override
+    public void DoStuff() {
+        System.out.println("TestClass");
+    }
+    
+}
